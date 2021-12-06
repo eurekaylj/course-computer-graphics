885edfef671f58b01407cda6dbb51b7096a77e30
eurekaylj <galileoyulj@gmail.com>
2021-12-06 15:06:50 +0800

    :art: modify lab3

--- CGlab3/CGlab3/main.cpp
+++ CGlab3/CGlab3/main.cpp
@@ -1,4 +1,3 @@
-//openGL-基于编码的剪裁算法Cohen-Sutherland
 #include <GL/glut.h>
 #include <math.h>
 #define PI 3.14159
@@ -20,26 +19,26 @@ void init()
     glClear(GL_COLOR_BUFFER_BIT);
 }
 
-void gl_Point(int x, int  y) {
-    glPointSize(5);
+// OpenGL画点
+void point(int x, int  y) {
     glBegin(GL_POINTS);
     glVertex2i(x, y);
     glEnd();
     glFlush();
 }
 
+// OpenGL画圆
 void circle(double x, double y)
 {
     int n = 150;
     double R = 15;
-    glColor3i(1, 1, 1);
     for (int i = 0; i < 150; i++)
     {
-        gl_Point(x + R * cos(2 * PI * i / n), y + R * sin(2 * PI * i / n));
+        glColor3i(0, 1, 0);
+        point(x + R * cos(2 * PI * i / n), y + R * sin(2 * PI * i / n));
     }
 }
 
-// DDA算法画直线
 void DDA(int x1, int y1, int x2, int y2) {
     int k, i;
     float x, y, dx, dy;
@@ -52,11 +51,12 @@ void DDA(int x1, int y1, int x2, int y2) {
     x = float(x1);
     y = float(y1);
     for (i = 0; i < k; i++) {
-        gl_Point(int(x + 0.5), int(y + 0.5));
+        point(int(x + 0.5), int(y + 0.5));
         x = x + dx;
         y = y + dy;
     }
 }
+
 
 // 编码
 int makecode(double x, double y)
@@ -69,40 +69,54 @@ int makecode(double x, double y)
     return c;
 }
 
-// 裁剪
+// 基于编码的Cohen-Sutherland剪裁算法
 void Cohen_Sutherland(double x0, double y0, double x2, double y2)
 {
     int c, c0, c1;
     double x, y;
+    // 给两个端点编码
     c0 = makecode(x0, y0);
     c1 = makecode(x2, y2);
+
+    // 若c0 | c1 = 0，对应直线段可简取之，否则继续判断
     while (c0 != 0 || c1 != 0)
     {
+        // 若c0 & c1 ！= 0， 对应直线段可简弃之
         if ((c0 & c1) != 0) break;
+        // 判断第一个顶点
         c = c0;
+        // 第一个顶点在内部，则判断第二个顶点
         if (c == 0) c = c1;
+        // 若顶点在左边，则直线段和左边界的交点
         if ((c & 1) == 1) {
             y = y0 + (y2 - y0) * (XL - x0) / (x2 - x0);
             x = XL;
         }
+        // 若顶点在右边，则求出直线段和右边界的交点
         else if ((c & 2) == 2) {
             y = y0 + (y2 - y0) * (XR - x0) / (x2 - x0);
             x = XR;
         }
+        // 若顶点在下边，则求出直线段和下边界的交点
         else if ((c & 4) == 4) {
             x = x0 + (x2 - x0) * (YD - y0) / (y2 - y0);
             y = YD;
         }
+        // 若顶点在上边，则求出直线段和上边界的交点
         else if ((c & 8) == 8) {
             x = x0 + (x2 - x0) * (YU - y0) / (y2 - y0);
             y = YU;
         }
+        // 用圆标注求出的交点
         circle(x, y);
+
+        // 替代第一个顶点
         if (c == c0) {
             x0 = x;
             y0 = y;
             c0 = makecode(x, y);
         }
+        // 替代第二个顶点
         else {
             x2 = x;
             y2 = y;
